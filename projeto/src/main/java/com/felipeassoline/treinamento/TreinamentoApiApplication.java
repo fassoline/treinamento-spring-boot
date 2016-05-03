537cc7e5ab9ac6d48540e4aaa6b9735eab467df4
Felipe Assoline <fassoline@gmail.com>
2016-05-03 19:08:10 -0300

    projeto base

---
+++ projeto/src/main/java/com/felipeassoline/treinamento/TreinamentoApiApplication.java
@@ -0,0 +1,12 @@
+package com.felipeassoline.treinamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentoApiApplication.class, args);
+	}
+}
